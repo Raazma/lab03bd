15c3f5637220ecd8b666f72aea30be30e8eef6fd
fredpaquette <frederic_paquette@hotmail.com>
2015-02-12 22:02:12 -0500

    finir rechercher et insert

--- LAB03Bd/LAB03Bd/Form1.cs
+++ LAB03Bd/LAB03Bd/Form1.cs
@@ -43,20 +43,21 @@ namespace LAB03Bd
        private void Form1_Load(object sender, EventArgs e)
        {
           Connection();
+          Filledliste();
           Filled();
        }
 
        private void Filled()
        {
-          OracleCommand ObjSelct = new OracleCommand("Employes", oraconn);
-         
+           Clear();
+          OracleCommand ObjSelct = new OracleCommand("Employes", oraconn);       
           ObjSelct.CommandType = CommandType.StoredProcedure;
           ObjSelct.CommandText = " GESTIONEMPLOYES.LISTER";
           OracleParameter liste = new OracleParameter("liste", OracleDbType.RefCursor);
           liste.Direction = ParameterDirection.ReturnValue;
           OracleParameter codedep = new OracleParameter("code", OracleDbType.Char, 3);
           codedep.Direction = ParameterDirection.Input;
-          codedep.Value = "INF";
+          codedep.Value = Lb_Codedep.SelectedItem.ToString();
           ObjSelct.Parameters.Add(liste);
           ObjSelct.Parameters.Add(codedep);
          // ObjSelct.ExecuteNonQuery();
@@ -67,12 +68,28 @@ namespace LAB03Bd
        }
       private void BindInfo()
        {
+         
           lBL_NumEmp.DataBindings.Add("Text", data, "lister.numemp");
           Tb_Nom.DataBindings.Add("Text", data, "lister.nomemp");
           Tb_Prenon.DataBindings.Add("Text", data, "lister.prenomemp");
           Tb_Sal.DataBindings.Add("Text", data, "lister.salaireemp");               
       }
 
+      private void Filledliste()
+      { 
+      
+              string sql = "select codedep from EMPLOYESCLG";
+            OracleCommand oraclecomm = new OracleCommand(sql, oraconn);
+
+            
+          OracleDataReader oraread = oraclecomm.ExecuteReader();
+           while (oraread.Read())
+           {             
+               Lb_Codedep.Items.Add(oraread.GetString(0));
+           }
+
+           Lb_Codedep.SelectedIndex = 1;
+      }
       private void Btn_suivant_Click(object sender, EventArgs e)
       {
          this.BindingContext[data, "lister"].Position += 1;
@@ -82,5 +99,84 @@ namespace LAB03Bd
       {
          this.BindingContext[data, "lister"].Position -= 1;
       }
+
+      private void Lb_Codedep_SelectedIndexChanged(object sender, EventArgs e)
+      {
+          Filled();
+      }
+      private void Clear()
+      {
+          data.Clear();
+          lBL_NumEmp.DataBindings.Clear();
+          Tb_Nom.DataBindings.Clear();
+          Tb_Prenon.DataBindings.Clear();
+          Tb_Sal.DataBindings.Clear();       
+      }
+
+      private void Btn_supprimer_Click(object sender, EventArgs e)
+      {
+          OracleCommand ObjSelct = new OracleCommand("Delete", oraconn);
+          ObjSelct.CommandType = CommandType.StoredProcedure;
+          ObjSelct.CommandText = " GESTIONEMPLOYES.SUPPRIMER";
+          OracleParameter codeemp = new OracleParameter("numemp",OracleDbType.Int32);
+          codeemp.Direction = ParameterDirection.Input;
+          codeemp.Value = lBL_NumEmp.Text;
+          ObjSelct.Parameters.Add(codeemp);
+         int i = ObjSelct.ExecuteNonQuery();
+
+         MessageBox.Show(i.ToString());
+      }
+
+      private void Btn_update_Click(object sender, EventArgs e)
+      {
+          OracleCommand ObjSelct = new OracleCommand("update", oraconn);
+          ObjSelct.CommandType = CommandType.StoredProcedure;
+          ObjSelct.CommandText = " GESTIONEMPLOYES.MJOUR";
+          OracleParameter codeemp = new OracleParameter("numemp", OracleDbType.Int32);
+          OracleParameter aug = new OracleParameter("aug", OracleDbType.Int32);
+          codeemp.Direction = ParameterDirection.Input;
+          aug.Direction = ParameterDirection.Input;
+          aug.Value = Tb_Sal.Text;
+          codeemp.Value = lBL_NumEmp.Text;
+          ObjSelct.Parameters.Add(codeemp);
+          ObjSelct.Parameters.Add(aug);
+          int i = ObjSelct.ExecuteNonQuery();
+
+          MessageBox.Show(i.ToString());
+      }
+
+      private void Btn_Nbpersonne_Click(object sender, EventArgs e)
+      {
+          OracleCommand ObjSelct = new OracleCommand(" NBEMPLOYES", oraconn);
+          ObjSelct.CommandType = CommandType.StoredProcedure;
+          ObjSelct.CommandText = " GESTIONEMPLOYES.NBEMPLOYES";
+          OracleParameter codeemp = new OracleParameter("numemp", OracleDbType.Int32);
+        
+          codeemp.Direction = ParameterDirection.ReturnValue;
+     
+          ObjSelct.Parameters.Add(codeemp);
+        
+           ObjSelct.ExecuteNonQuery();
+
+          MessageBox.Show("il y a "+codeemp.Value.ToString() + " employes");
+       }
+
+      private void Btn_recherche_Click(object sender, EventArgs e)
+      {
+          OracleCommand ObjSelct = new OracleCommand("Employes", oraconn);
+          ObjSelct.CommandType = CommandType.StoredProcedure;
+          ObjSelct.CommandText = " GESTIONEMPLOYES.RECHERCHER";
+          OracleParameter Result = new OracleParameter("liste", OracleDbType.RefCursor);
+         Result.Direction = ParameterDirection.Output;
+          OracleParameter codedep = new OracleParameter("code", OracleDbType.Varchar2, 30);
+          codedep.Direction = ParameterDirection.Input;
+          codedep.Value = Tb__Recherche.Text;
+          ObjSelct.Parameters.Add( Result);
+          ObjSelct.Parameters.Add(codedep);
+
+          ObjSelct.ExecuteNonQuery();
+          MessageBox.Show(Result.ToString());
+      }
+    }
    }
-}
+
